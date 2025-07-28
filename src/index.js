8251985290e968224642477bbc853fd691fcff3f
Gkmaster-coder <gouravkashyap2468@gmail.com>
2025-07-28 12:45:30 +0530

    Add Custom Api and Error handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
+    path: './env'
 })
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Your server is Running..... on ${process.env.PORT}`)
+        }
+        );
+    })
+    .catch((err) => {
+        console.log("Mongo db Connection failed !!!", err)
+    })
